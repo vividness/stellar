e7710e29c36252fac2b219f06387f30a989de6f7
Vladimir Ivic <mancmelou@me.com>
2013-03-20 08:13:26 -0700

    Adding bootstrap.php

--- www/index.php
+++ www/index.php
@@ -6,7 +6,7 @@ define('ROOT',      realpath(__DIR__ . '/..'));
 define('WWW_ROOT',  realpath(__DIR__));
 define('APP_ROOT',  realpath(__DIR__ . '/../app'));
 
-require_once ROOT . "/autoloader.php";
+require_once ROOT . "/bootstrap.php";
 
 $app = new Application();
 $app->run();
