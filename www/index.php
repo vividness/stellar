933db7127080c517f57bf0daab55ce1083e45473
Vladimir Ivic <mancmelou@me.com>
2013-03-20 08:26:02 -0700

    Use APP botstrap.php

--- www/index.php
+++ www/index.php
@@ -6,8 +6,7 @@ define('ROOT',      realpath(__DIR__ . '/..'));
 define('WWW_ROOT',  realpath(__DIR__));
 define('APP_ROOT',  realpath(__DIR__ . '/../app'));
 
-require_once ROOT . "/bootstrap.php";
+require_once APP_ROOT . "/bootstrap.php";
 
 $app = new Application();
 $app->run();
-
